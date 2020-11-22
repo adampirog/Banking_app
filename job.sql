f1239d3b6c2670722880f7fba0f8be3e5f496224
Adam <pirog.adam@gmail.om>
2020-11-22 11:45:12 +0100

    Add MySQL job comments

--- job.sql
+++ job.sql
@@ -1,27 +1,33 @@
+SET GLOBAL event_scheduler = ON;
+
 DROP EVENT IF EXISTS `Loans_payment`;
 delimiter |
 
 CREATE EVENT `Loans_payment` 
     ON SCHEDULE
-        EVERY 30 SECOND
+        EVERY 30 SECOND     -- before deployment change testing value to 30 DAY
     ENABLE
 DO BEGIN
 
+    -- calculate interest
     UPDATE loans
     SET loans.rateValue = ((loans.value/loans.installments) + (loans.value - (loans.value/loans.installments) * installmentsPaid) * interestRate)
     WHERE loans.status = 'open';
 
+    -- create record in repaymentRecords
     INSERT INTO repaymentRecords(loanId, paymentDate, amount)
     SELECT loans.id, NOW(), loans.rateValue
     FROM loans
     WHERE loans.status = 'open';
 
+    -- charge clients deposit
     UPDATE deposits
     INNER JOIN loans on loans.depositId = deposits.id
     SET deposits.balance = deposits.balance - loans.rateValue,
         loans.installmentsPaid = installmentsPaid + 1
     WHERE loans.status = 'open';
 
+    -- check for fully payed loans
     UPDATE loans
     SET loans.status = 'closed'
     WHERE loans.installmentsPaid >= loans.installments;
